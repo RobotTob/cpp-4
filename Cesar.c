2d4a35e55ec41179515da15f89460140ee52ad40
RobotTob <mathewt1@usf.edu>
2024-09-15 22:58:03 +0000

    cesar done

--- Cesar.c
+++ Cesar.c
@@ -8,32 +8,38 @@
 //NO LOOPS ALLOWED
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_LEN 1000
+
+FILE *clear, *secret;
 
 int main() {
-    char str[MAX_LEN];
-    FILE* pFile;
+     // These are variables for the txt files
 
-    // Open the file
-    // pFile = fopen("clearMessage.txt", "r");
-    // if (pFile == NULL) {
-    //     printf("Error opening file\n");
-    //     return 1;
-    // }
-    FILE *file;
-
-    if ((file = fopen("clearMessage.txt", "r")) == NULL){
-        printf("Error opening file\n");
-        exit(1);
+    // This opens the clearMessage txt file for reading
+    if ((clear = fopen("clearMessage.txt", "r")) == NULL) { //the "r" reads from the file of clearMessage.txt
+        printf("Error opening file\n"); //returns error of file didn't open
+        return -1;
     }
-    
+    // This opens a file called secretMessage txt file to write in
+    if ((secret = fopen("secretMessage.txt", "w")) == NULL) { //the "w" writes in the new file of secretMessage.txt
+        printf("Error opening output file\n"); //returns error of file didn't open
+        return -1;
+    }
 
-    // Read the file character by character
+    // Read the file character by character and apply Caesar Cipher
     int ch;
-    while ((ch = fgetc(file)) != EOF) {
-        putchar(ch);  // Print each character to the console
+    while ((ch = fgetc(clear)) != EOF) { //it returns error code EOF if the file's not closed right
+        // This is for charachters that are uppercase letter
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ((ch - 'A' + 3) % 26) + 'A';  // Shift within uppercase range
+        }
+        // This is for charachters that are lower letter
+        else if (ch >= 'a' && ch <= 'z') {
+            ch = ((ch - 'a' + 3) % 26) + 'a';  // Shift within lowercase range
+        }
+        // This prints the new characters into the new secret file
+        fprintf(secret, "%c", ch);
     }
 
-    fclose(file); // Close the file
-    return 0;
+    fclose(clear);  // Close the clearMessage.txt file
+    fclose(secret); // Close the secretMessage.txt file
 }
