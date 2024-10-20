ed752a1244a1cf93047e987e40fc74376e33f6aa
RobotTob <mathewt1@usf.edu>
2024-10-20 18:55:27 -0400

    starWars.c

--- mathTable.c
+++ mathTable.c
@@ -1,46 +1,87 @@
-//Write a program in C to display the multiplication table for numbers 1-12 in a square table. just display it.
-//Create a number guessing game that sets a number and then allows the user to guess it. 
-//The number will be between 1 and 10
-//Hint: 
-// To calculate a random number between 1-10 for the game use:
-// int guessAnswer =
-// (rand() % 10) + 1; //
-// random number
-#include <stdio.h> //this for print statemets
-#include <stdlib.h> //this is necessary for random number generation
-#include <time.h> //This is to seed a random number every run
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main () {
-    int guessAnswer; //the random number is an integer
-    int guess; //this is the user's guess
+#define MAX_LEN 256
+#define NUM_YEARS 43
 
-    printf("Multiplication Table: \n");
-    //The for loop below works as the following:
-    /* The first loop has the first set of numbers
-     when the first number starts in i it multilies that number from 1-12 in the 2nd for loop.
-     after that i has gone through all 12 Js the i becomes a 2 and then that 2 gets multiplied from a new set of 12 Js form 1-12.
-     Then it does that again and again until i reaches 12 and the 12th i reaches its 12th j. Giving 12x12 = 144.*/
-    for (int i = 1; i <= 12; i++) { 
-        for (int j = 1; j <= 12; j++) {
-            printf("%4d", i * j); // This spaces it out a bit
-        }
-    printf("\n"); //this is for a new line after each increment of i is done.
+//This finds the length of the titles
+int get_title_length(const char* title) {
+    return strlen(title);
+}
+
+int main() {
+    FILE *file = fopen("AHPA #12 - Movie Data.txt", "r");
+    if (!file) { //this is if is null
+        printf("Error opening file.\n");
+        return 1;
     }
 
-    srand(time(NULL)); //This randomizes the number every run. Without this the number is always 4.
-    guessAnswer = (rand() % 10) + 1; //random number generator for #s between 1-10
+    int year;
+    double total_gross, average;
+    int releases;
+    char title[MAX_LEN];
 
-    printf("\nEnter your guess (1-10):\n");
-    scanf("%d", &guess);
-    //this while loop runs as long as the guess isn't the same as the random number
-    while (guess != guessAnswer){
-        if (guess > guessAnswer){
-            printf("Too high\n");
-        } else{
-            printf("Too low!\n");
-        }
-        scanf("%d", &guess); //the "&" is so scanf knows where to store the value at.
+    //THis stores the average lenght of the tieles
+    double income_by_title_length[MAX_LEN] = {0};
+    int title_length_count[MAX_LEN] = {0};
+
+    //These are the movies we look for
+    const char* star_wars_titles[] = {
+        "Star Wars: Episode III - Revenge of the Sith",
+        "Star Wars: Episode I - The Phantom Menace",
+        "Star Wars: Episode VI - Return of the Jedi",
+        "Star Wars: Episode V - The Empire Strikes Back",
+        "Star Wars: Episode IV - A New Hope"
+    };
+    
+    //stores the Star Wars titles
+    const char* star_wars_pointers[5];
+
+    for (int i = 0; i < 5; i++) {
+        star_wars_pointers[i] = star_wars_titles[i];
     }
-    printf("You guessed it");//It prints this if the guess is correct
-    return 0; //after the correct guess has been guessed the program ends
+
+    // This skips the header
+    char header[MAX_LEN];
+    fgets(header, MAX_LEN, file);
+
+    // This reads each line from the file
+    while (fscanf(file, "%d %lf %d %lf %[^\n]s", &year, &total_gross, &releases, &average, title) != EOF) {
+        int title_length = get_title_length(title);
+        
+        //This dds the average income to the corresponding title length
+        income_by_title_length[title_length] += average;
+        title_length_count[title_length] += 1;
+    }
+
+    fclose(file);
+
+    //This finds the title length that made the most money
+    int max_length = 0;
+    double max_income = 0;
+    for (int i = 0; i < MAX_LEN; i++) {
+        if (income_by_title_length[i] > max_income) {
+            max_income = income_by_title_length[i];
+            max_length = i;
+        }
+    }
+
+    //This gives the output the income for each title length
+    printf("Average income by movie title length:\n");
+    for (int i = 0; i < MAX_LEN; i++) {
+        if (title_length_count[i] > 0) {
+            printf("Title length %d: $%.2f\n", i, income_by_title_length[i]);
+        }
+    }
+
+    printf("\nThe movie title length that made the most money was %d with $%.2f\n\n", max_length, max_income);
+
+    //This outputs the Star Wars movies
+    printf("Star Wars Movies in List:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d. %s\n", i, star_wars_pointers[i]);
+    }
+
+    return 0;
 }
